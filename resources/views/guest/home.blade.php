aba4a32c2073e8646478568e128fc9ab211fd243
fathergasc <quaglioniandrea@gmail.com>
2022-10-07 16:25:17 +0200

    add: App.vue, guest.home

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,5 +1,34 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <title>Frontend</title>
+</head>
+<body>
 
-@section('content')
-    homepage guest
-@endsection
+    <div id="root">
+        <h1>Welcome to guests Homepage</h1>
+
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+    </div>
+
+    <script src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
